eabcac58bf790eea91b3855d1bacee7eeb1af6a1
PanCytryn <pancytryn.dawid@gmail.com>
2025-01-20 10:38:03 +0100

    end

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const mysql = require('mysql')
+
+const app = express()
+
+var conn = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "2PROgr2"
+});
+
+conn.connect((err) => {
+  if (err){
+    console.log("Nie połączono z bazą danych");
+    console.log(err);
+  } else {
+    console.log("Połączono z bazą danych");
+  }
+
+})
+
+app.get('/', (req, res) => {
+  res.send("test")
+})
+
+app.listen(3000, () => {
+  console.log('Serwer uruchomiony na porcie 3000')
+})
